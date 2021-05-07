6b1f13ac2f37e402a3c59b9d4d54a0e1a7444590
pragnesh <pg.radadia@gmail.com>
2021-05-07 17:55:54 +0530

    add TranscationService class

---
+++ src/main/java/com/batterbank/be/service/TransactionService.java
@@ -0,0 +1,12 @@
+package com.batterbank.be.service;
+
+import com.batterbank.be.domain.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionService {
+    List<Transaction> findAllByAccountNumber(Long accountNumber) {
+        return new ArrayList<>();
+    }
+}
