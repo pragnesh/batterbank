44e13cedb204b25b4fbff1aa442803d5b2d1521e
pragnesh <pg.radadia@gmail.com>
2021-05-10 19:16:18 +0530

    enable redis cache

--- src/main/java/com/batterbank/be/repositery/TransactionRepository.java
+++ src/main/java/com/batterbank/be/repositery/TransactionRepository.java
@@ -1,10 +1,12 @@
 package com.batterbank.be.repositery;
 
 import com.batterbank.be.domain.Transaction;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface TransactionRepository extends CrudRepository<Transaction, Long> {
+    @Cacheable("transaction")
     List<Transaction> findByAccountNumber(Long accountNumber);
 }
