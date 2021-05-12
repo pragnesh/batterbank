808262f2389e8d48913e37884431c27ffbf308fd
pragnesh <pg.radadia@gmail.com>
2021-05-12 15:14:33 +0530

    adding docker compose file & related changes

--- src/main/java/com/batterbank/be/config/CachingConfig.java
+++ src/main/java/com/batterbank/be/config/CachingConfig.java
@@ -1,19 +1,10 @@
 package com.batterbank.be.config;
 
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
 @EnableCaching
 public class CachingConfig {
-
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("172.17.0.3", 6379));
-    }
 
 }
